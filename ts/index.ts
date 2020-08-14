b90338d0adc540cbc546f1443dbafc9d7668a75f
Francisco Flores <fflores.ic.ipn@hotmail.com>
2020-08-14 16:01:47 -0500

    Estructura y funciones

--- ts/index.ts
+++ ts/index.ts
@@ -38,3 +38,57 @@ comodin = { type: 'Wildcard' };
 
 // Object
 let someObject: object = { type: 'Wildcard' };
+
+// Funciones
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+const sum = add(4, 6);
+
+function createAdder(a: number): (number) => number {
+  return function(b: number) {
+    return b + a;
+  };
+}
+
+const addFour = createAdder(4);
+const fourPlus6 = addFour(6);
+
+function fullName(firstName: string, lastName: string = 'Smith'): string {
+  return `${firstName} ${lastName}`;
+}
+
+const richard = fullName('Agente');
+console.log(richard);
+
+// Interfaces
+enum Color {
+  Rojo = 'Rojo',
+  Verde = 'Verde',
+}
+
+interface Rectangulo {
+  ancho: number;
+  alto: number;
+  color?: Color;
+}
+
+let rect: Rectangulo = {
+  ancho: 4,
+  alto: 6,
+  // color: Color.Rojo,
+};
+
+function area(r: Rectangulo): number {
+  return r.alto * r.ancho;
+}
+
+const areaRect = area(rect);
+console.log(areaRect);
+
+rect.toString = function() {
+  return this.color ? `Un rectangulo ${this.color}` : `Un rectangulo`;
+};
+
+console.log(rect.toString());
