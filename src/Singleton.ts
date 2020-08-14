0e3a6524b922ec695be9a046ddb97503bb6eb384
Francisco Flores <fflores.ic.ipn@hotmail.com>
2020-08-14 17:25:46 -0500

    Ejemplo de Singleton

---
+++ src/Singleton.ts
@@ -0,0 +1,15 @@
+export default class Singleton {
+    private static instance: Singleton;
+
+    private constructor() {
+
+    }
+
+    static getInstance() {
+        if (!Singleton.instance) {
+            Singleton.instance = new Singleton();
+        }
+
+        return Singleton.instance;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -94,3 +94,10 @@ rect.toString = function() {
 };
 
 console.log(rect.toString());
+
+import Singleton from './Singleton';
+
+const a = Singleton.getInstance();
+const b = Singleton.getInstance();
+
+console.log('¿A es igual a B?', a === b);
