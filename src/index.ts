c143e2c7c89111e5e21a9cb236e6d8d499750f1b
Francisco Flores <fflores.ic.ipn@hotmail.com>
2020-08-14 17:50:18 -0500

    Implementación de Patrón Observer

--- src/index.ts
+++ src/index.ts
@@ -101,3 +101,57 @@ const a = Singleton.getInstance();
 const b = Singleton.getInstance();
 
 console.log('¿A es igual a B?', a === b);
+
+interface Observer {
+  update: (data: any) => void;
+}
+
+interface Subject {
+  subscribe: (observer: Observer) => void;
+  unsubscribe: (observer: Observer) => void;
+}
+
+class XRPPrice implements Subject {
+  observers: Observer[] = [];
+
+  constructor () {
+    const element: HTMLInputElement = document.querySelector("#value");
+    element.addEventListener('input', () => {
+      this.notify(element.value);
+    });
+  }
+
+  subscribe (observer: Observer) {
+    this.observers.push(observer);
+  };
+
+  unsubscribe (observer: Observer) {
+    const index = this.observers.findIndex(obs => {
+      return obs === observer;
+    })
+    this.observers.splice(index, 1);
+  };
+
+  notify(data: any) {
+    this.observers.forEach(observer => observer.update(data));
+  }
+}
+
+class PriceDisplay implements Observer {
+  private element: HTMLElement;
+
+  constructor() {
+    this.element = document.querySelector("#price");
+  }
+
+ update(data: any) {
+  this.element.innerText = data;
+ };
+}
+
+const value = new XRPPrice();
+const display = new PriceDisplay();
+
+value.subscribe(display);
+
+setTimeout(() => value.unsubscribe(display), 3000);
